ce106b5520b693695bd0b92c79bbd306298fbc84
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-10-26 15:35:36 +0600

    fakedata loaded

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const mycourses = require('./data/fakedata.json');
+
 
 app.get("/", (req, res) => {
     res.send('News api running');
+})
+app.get("/mycourses", (req, res) => {
+    res.send(mycourses);
 })
 
 app.listen(port, () => {
